be224be86d29dd6d0dc86719a7a239c7151749d6
KeanuXKnight <keanuroush@gmail.com>
2016-02-15 10:46:28 -0800

    addarray

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 
 
 function  sum(N1, N2) {
+  //console.log('sum is' + sum);
    return N1 + N2;
+
 }
+
+function multiply(N1, N2) {
+  //console.log('the total is' + );
+  return N1 * N2;
+
+}
+
+function sumAndMultiply(N1, N2, N3) {
+  var sum3 = sum(sum(N1, N2), N3)
+  console.log(sum3);
+
+  var mult3 = multiply(multiply(N1,N2),N3 )
+  console.log(mult3);
+  return [sum3, mult3]
+}
+
+function sumArray(numArray) {
+  var totalSoFar = 0;
+    for (var i = 0; i < numArray.length; i++) {
+      totalSoFar = numArray[i] + totalSoFar;
+      console.log(totalSoFar);
+    }
+  return totalSoFar
+  }
+
+  

5602ab1e4d5d82d5167392e698573ac451132e9e
KeanuXKnight <keanuroush@gmail.com>
2016-02-15 09:50:50 -0800

    add mult

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 
 
 function  sum(N1, N2) {
+  console.log('sum is' + sum);
    return N1 + N2;
+
+}
+
+function multiply(N1, N2) {
+  console.log('the total is' + multiply);
+  return N1 * N2;
+
+}
+function sumAndMultiply(N1, N2, N3) {
+  var sum3 = sum(sum(N1, N2), N3)
+  console.log(sum3);
+
+  var mult3 = multiply(multiply(N1,N2),N3 )
+  console.log(mult3);
+  return [sum3, mult3]
 }
